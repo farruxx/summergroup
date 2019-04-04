4973d243cff2a2b1c03c04de1184e6a2e5c360e9
Farrux Bahodirov <farruxx@bk.ru>
2019-04-04 16:24:12 +0500

    body

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,22 +1,29 @@
 const https = require('https');
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 export function handler(event, context, callback) {
-    // console.log("queryStringParameters", event.queryStringParameters)
-    // callback(null, {
-    //   // return null to show no errors
-    //   statusCode: 200, // http status code
-    //   body: JSON.stringify({
-    //     msg: "Hello, World! " + Math.round(Math.random() * 10),
-    //   }),
-    // })
-    const options = {
-      host: 'api.telegram.org',
-      path: '/bot672568768:AAEjxcuQ73tA5-fcNzQaVEr9WqFD5C2VvZY/sendMessage?chat_id=farruxtestchannel&text=[MY_MESSAGE_TEXT]',
-      method: 'GET'
+  // console.log("queryStringParameters", event.queryStringParameters)
+  // callback(null, {
+  //   // return null to show no errors
+  //   statusCode: 200, // http status code
+  //   body: JSON.stringify({
+  //     msg: "Hello, World! " + Math.round(Math.random() * 10),
+  //   }),
+  // })
+  const options = {
+    host: 'api.telegram.org',
+    path: '/bot672568768:AAEjxcuQ73tA5-fcNzQaVEr9WqFD5C2VvZY/sendMessage?chat_id=farruxtestchannel&text=[MY_MESSAGE_TEXT]',
+    method: 'GET'
   };
-
+  var body = ''
   const req = https.request(options, (res) => {
-    callback(JSON.stringify(res));
+    var body = '';
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+    res.on('end', function () {
+      console.log(body);
+      callback(body);
+    });
   });
 
   req.on('error', (e) => {
@@ -26,7 +33,7 @@ export function handler(event, context, callback) {
   // send the request
   req.write('');
   req.end();
-  }
+}
 
   // Now you are ready to access this API from anywhere in your Gatsby app! For example, in any event handler or lifecycle method, insert:
   // fetch("/.netlify/functions/hello")
